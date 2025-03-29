6dbc82a98c66e77fb49dddc0ce872982db59dbe0
Greatjobson <zumabejba@gmail.com>
2025-03-29 21:02:32 +0500

    added documentation

--- src/Recursion/T1.java
+++ src/Recursion/T1.java
@@ -9,16 +9,24 @@ public class T1 { // function that returns minimum of them
         int n = sc.nextInt(); //choose number for array lenght
 
         int[] arr = new int[n]; // create array
-
         for(int i = 0;i< arr.length;i++){
-            arr[i] = sc.nextInt(); // filling in the array
+            arr[i] = sc.nextInt();
         }
-
 
         System.out.println(minN(arr,0)); //calling func for result
     }
 
-    public static int minN (int[] arr,int s){ // function for find minimum
+    /*
+    *This method calculates the minimum for a given array.
+    *It uses a recursive approach.
+    *The recursive algorithm iterates through all numbers in array
+    *
+    *Time complexity: 0(n)
+    *@param arr Array to search for
+    *@param s Current index (start from 0)
+    *@return Minimum from the array.
+    */
+    public static int minN (int[] arr,int s){
         if(arr.length - 1 == s){
             return arr[s]; //base condition
         }
